8e14b9fac5d622cba0dfca8629267199eb0c91e8
mgredziak <mgredziak@gmail.com>
2014-11-30 21:13:18 +0100

    haha

--- GmmediaWebNodeApp/server.js
+++ GmmediaWebNodeApp/server.js
@@ -18,6 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
  
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function () {
+
   console.log('Express server listening on port ' + app.get('port'));
 });
